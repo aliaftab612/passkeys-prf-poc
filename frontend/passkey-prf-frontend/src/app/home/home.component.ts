713abca1641414931f86f3b57013afc0ce62b500
Aftab Ali <aliaftab612@gmail.com>
2024-07-30 22:41:12 +0530

    completed homepage functionality

--- frontend/passkey-prf-frontend/src/app/home/home.component.ts
+++ frontend/passkey-prf-frontend/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
 import { encryptString, decryptString } from '../utility/cryptoHelper';
@@ -13,26 +13,40 @@ import { DataStorageService } from '../services/data-storage.service';
   styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit {
+  @ViewChild('form')
+  form!: NgForm;
   encryptedString: string = '';
   decryptedString: string = '';
   inputValue: string = '';
-  resetClicked: boolean = false;
+  prfNotSupported: boolean = false;
 
   constructor(
     private authService: AuthService,
     private dataStorageService: DataStorageService
   ) {}
 
-  async ngOnInit() {
+  ngOnInit() {
+    this.initializeData();
+  }
+
+  async initializeData() {
+    if (
+      !this.authService.getEncryptionKey() &&
+      this.authService.getAuthToken()
+    ) {
+      this.prfNotSupported = true;
+      return;
+    }
+
     this.encryptedString = await this.dataStorageService.getEncryptedString();
   }
 
-  async encrypt(form: NgForm) {
-    if (form.valid) {
+  async encrypt() {
+    if (this.form.valid) {
       const encryptionKey = this.authService.getEncryptionKey();
       if (encryptionKey) {
         const encryptedString = await encryptString(
-          form.value.userinput,
+          this.form.value.userinput,
           encryptionKey
         );
 
@@ -59,7 +73,6 @@ export class HomeComponent implements OnInit {
   }
 
   resetClick() {
-    this.resetClicked = true;
     this.inputValue = '';
     this.decryptedString = '';
   }
