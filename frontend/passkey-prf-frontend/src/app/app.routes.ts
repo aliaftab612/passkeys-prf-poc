2949dda43f64a78c9d474385682cc9c6e01f3b73
Aftab Ali <aliaftab612@gmail.com>
2024-08-03 15:50:01 +0530

    Added Auth Guard

--- frontend/passkey-prf-frontend/src/app/app.routes.ts
+++ frontend/passkey-prf-frontend/src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { HomeComponent } from './home/home.component';
+import { AuthGuard } from './login/auth.guard';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: '', pathMatch: 'full', redirectTo: 'login' },
-  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
 ];
